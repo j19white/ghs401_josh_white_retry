aaab684f44ee54b03f3321493714c7a32c9662e3
Joshua White <joshua.whi@lvjusd.k12.ca.us>
2018-12-03 13:33:34 -0800

    added bluej folder

---
+++ Random Stuff/RandomStuff.java
@@ -0,0 +1,15 @@
+
+
+public class RandomStuff
+{
+    // instance variables - replace the example below with your own
+    public static void main(){
+        
+        for(int i=0; i < 5; i++){
+        double age = (int)(Math.random()*(8));
+        System.out.println("The number is " + (age+367) + "!");
+        
+        
+    }
+}
+}
