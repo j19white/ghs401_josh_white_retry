0119ce954ee0a00317d2d375b1aa530021192cff
Joshua White <joshua.whi@lvjusd.k12.ca.us>
2018-12-04 12:39:50 -0800

    updated

--- Random Stuff/RandomStuff.java
+++ Random Stuff/RandomStuff.java
@@ -5,6 +5,7 @@ public class RandomStuff
     // instance variables - replace the example below with your own
     public static void main(){
         
+        System.out.println("Hi");
         for(int i=0; i < 5; i++){
         double age = (int)(Math.random()*(8));
         System.out.println("The number is " + (age+367) + "!");
